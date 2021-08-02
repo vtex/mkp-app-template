b589554e1f3ae73243efbc7d3cc01c9691ce9fe7
Felipe Weiss <felipe.weiss@vtex.com.br>
2021-08-02 17:19:48 -0300

    Getting ConectorName from appTitle

--- node/constants/variables.ts
+++ node/constants/variables.ts
@@ -1,4 +1,4 @@
-export const CONNECTOR_NAME = '{{appName}}'
+export const CONNECTOR_NAME = '{{appTitle}}'
 export const CONNECTOR_ENDPOINT = '{{conectorEndpoint}}'
 export const VBASE_BUCKET = '{{appName}}'
 export const VBASE_CONFIG_BASE_PATH = 'config'
