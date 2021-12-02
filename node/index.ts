9b7759e6e464ccac13fe562e165cdcc376eaeeca
Felipe Weiss <felipe.weiss@vtex.com.br>
2021-12-02 18:31:44 -0300

    Adding listener to order status update

--- node/index.ts
+++ node/index.ts
@@ -1,8 +1,9 @@
-import type { ClientsConfig, ServiceContext } from '@vtex/api'
+import type { ClientsConfig, ServiceContext, RecorderState, EventContext } from '@vtex/api'
 import { LRUCache, Service } from '@vtex/api'
 import { mapObjIndexed } from 'ramda'
 
 import { Clients } from './clients'
+import { someStates } from './middlewares/someStates'
 import { mutations, queries } from './resolvers'
 import { getConfig } from './routes/getConfig'
 import { getConnectorConfig } from './routes/getConnectorConfig'
@@ -38,11 +39,27 @@ const clients: ClientsConfig<Clients> = {
 declare global {
   // We declare a global Context type just to avoid re-writing ServiceContext<Clients, State> in every handler and resolver
   type Context = ServiceContext<Clients>
+
+  interface StatusChangeContext extends EventContext<Clients> {
+    body: {
+      domain: string
+      orderId: string
+      currentState: string
+      lastState: string
+      currentChangeDate: string
+      lastChangeDate: string
+    }
+  }
+  // The shape of our State object found in `ctx.state`. This is used as state bag to communicate between middlewares.
+  interface State extends RecorderState {}
 }
 
 // Export a service that defines route handlers and client options.
 export default new Service({
   clients,
+  events: {
+    someStates,
+  },
   graphql: {
     resolvers: {
       Query: {
---
+++ node/middlewares/someStates.ts
@@ -0,0 +1,9 @@
+export async function someStates(
+  ctx: StatusChangeContext,
+  next: () => Promise<any>
+) {
+
+  console.log(ctx.body)
+
+  await next()
+}
