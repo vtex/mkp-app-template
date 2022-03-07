8bea38afcf48cb95cddb3a091a1ad0ebc39ec1e9
Luiza Engler <luiza.engler@vtex.com.br>
2022-03-07 19:44:02 -0300

    Fix lint

--- node/clients/connector.ts
+++ node/clients/connector.ts
@@ -10,13 +10,12 @@ export default class ConnectorClient extends ExternalClient {
   }
 
   public async notifyConnectorAppUpdate(config: Configuration) {
-    var expression = /[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)?/gi;
-    var regex = new RegExp(expression);
+    var expression = /[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)?/gi
+    var regex = new RegExp(expression)
 
     if (CONNECTOR_ENDPOINT.match(regex)) {
-      config.accountName = this.context.account,
-        this.http.post("store-config/notification", config)
+      config.accountName = this.context.account
+      this.http.post("store-config/notification", config)
     }
-
   }
 }
