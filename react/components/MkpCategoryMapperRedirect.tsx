dbde1edc8059b4a4bc159653534049dced4c93ee
Talita <talita.fernandes@vtex.com.br>
2022-03-28 10:06:21 -0300

    Update MkpCategoryMapperRedirect.tsx

--- react/components/MkpCategoryMapperRedirect.tsx
+++ react/components/MkpCategoryMapperRedirect.tsx
@@ -1,5 +1,5 @@
 import { Spinner } from '@vtex/admin-ui'
-import type { FC } from 'react'
+import { FC, useState } from 'react'
 import React from 'react'
 import { useQuery } from 'react-apollo'
 import getConfig from '../graphql/getConfig.gql'
@@ -8,24 +8,33 @@ import { useRuntime } from 'vtex.render-runtime'
 
 const MkpCategoryMapperRedirect: FC = () => {
   const { navigate } = useRuntime()
-  console.log("here")
+  const [idNotFound, setIdNotFound] = useState(false)
   const { data } = useQuery(getConfig, {
     onCompleted: () => {
       console.log(data)
       if (!!data.getConfiguration.mapperId )
-      navigate({
-        page: 'admin.app.mkp-category-mapper',
-        params: {
-          marketplaceId: data.getConfiguration.mapperId,
-        },
-      })
+        navigate({
+          page: 'admin.app.mkp-category-mapper',
+          params: {
+            marketplaceId: data.getConfiguration.mapperId,
+          },
+        })
+      else
+        setIdNotFound(true)
     },
   })
+
+  if (idNotFound)
+  {
+    return (<div>{"Mapper Id not found, please "}</div>)
+  }
+
   return (
 
     <div className="w-100 vh-50 flex items-center justify-center">
       <div className="w-100 flex justify-center">
-        <Spinner />
+        {idNotFound ? (<div>{"Mapper Id not found, please activate the integration"}</div>) :<Spinner /> }
+
       </div>
     </div>
   )
