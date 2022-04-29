ff40fc36404a2dfadb841244fb1f507bc72088f9
Felipe Weiss <felipe.weiss@vtex.com.br>
2022-04-29 15:22:48 -0300

    adding tooltip to affiliate field and fixing Affiliate component

--- react/areas/ConfigArea/DefaultConfigs/affiliate.tsx
+++ react/areas/ConfigArea/DefaultConfigs/affiliate.tsx
@@ -3,7 +3,11 @@ import React from 'react'
 import InputComponent from '../../../components/InputComponent'
 import type { DefaultProps } from '../../../typings/props'
 
-const Affiliate: React.FC<DefaultProps> = ({ intl, config }) => {
+export interface AffiliateProps extends DefaultProps {
+  setConfig: React.Dispatch<React.SetStateAction<Configuration>>
+}
+
+const Affiliate: React.FC<AffiliateProps> = ({ intl, config, setConfig }) => {
   return (
     <InputComponent
       id="affiliateId"
@@ -11,6 +15,13 @@ const Affiliate: React.FC<DefaultProps> = ({ intl, config }) => {
       canEdit={true}
       initValue={config.affiliateId}
       type="text"
+      required
+      tooltip={intl.formatMessage({ id: 'admin/app.affiliateId.tooltip' })}
+      onChange={(value) => {
+        if (value !== undefined) {
+          setConfig((oldConfig) => ({ ...oldConfig, affiliateId: value }))
+        }
+      }}
     />
   )
 }
